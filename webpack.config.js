d5cb041a8714bc70edc40e32d6ffc47e3a276a8b
ant0-nn <anton.250605@gmail.com>
2023-06-30 18:44:18 +0300

    Finish base framework functional

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,8 @@ const isLoader = () => {
     {
       loader: 'babel-loader',
       options: {
-        presets: ['@babel/preset-env']
+        presets: ['@babel/preset-env'],
+        plugins: ['@babel/plugin-proposal-class-properties']
       }
     }
   ];
